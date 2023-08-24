3b6b605b5c80e3f893f0c524f61fd68d1e2bef05
AnercioRibeiro <anercio.ribeiro@outlook.com>
2023-08-24 22:15:49 +0100

    Add Post and Get in the Controller

---
+++ API/Controllers/PublicationController.cs
@@ -0,0 +1,37 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PublicationController : ControllerBase
+    {
+        public readonly ApplicationDbContext _dbContext;
+        public PublicationController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public ActionResult GetPublications()
+        {
+            var publications = _dbContext.Publications.ToList();
+            return Ok(publications);
+        }
+        [HttpPost]
+        public ActionResult Post(Publication publication)
+        {
+            Publication pub = new()
+            {
+                Identifier = publication.Identifier,
+                CreatedDate = DateTime.Now,
+                UpdatedDate = DateTime.Now
+            };
+            _dbContext.Publications.Add(pub);
+            _dbContext.SaveChanges();
+            return Ok(pub);
+        }
+    }
+}
